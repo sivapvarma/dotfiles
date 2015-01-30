1f958f98c1bdab957289882c3dca3b1c2af3b88b
Siva Prasad Varma <sivapvarma@gmail.com>
2015-01-31 01:13:48 +0530

    added tabbed layout
    still a lot of xmonad to explore

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,19 +4,52 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 import System.IO
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.UrgencyHook
+import XMonad.Layout.Grid
+import XMonad.Layout.ResizableTile
+import XMonad.Util.Themes
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.SimplestFloat
+
+myModMask               = mod1Mask      -- changes the mod to left Alt Key
+myFocusedBorderColor    = "#00FF00"
+myNormalBorderColor     = "#CCCCCC"
+myBorderWidth           = 1
+myTerminal              = "xterm"       -- Solarized colorschemed
+
+{-
+ Workspace configuraiton
+-}
+myWorkspaces = ["0:Term", "1:Web", "2:Dev", "3:Docs", "4:Pix", "5:Float"]
+startupWorkspace = "2:Dev"      -- which workspace do you want to be on after launch
+
+-- , layoutHook  = avoidStruts  $  layoutHook defaultConfig
+defaultLayouts = smartBorders(avoidStruts(
+    ResizableTall 1 (3/100) (1/2) []
+    ||| tabbed shrinkText (theme deiflTheme)--defaultTheme
+    ||| noBorders Full
+    ||| Grid))
+
+myLayouts =
+    onWorkspace "5:Float" simplestFloat
+    $ defaultLayouts
 
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar /home/spv/.xmonad/xmobar.hs"
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
     spawn "xrdb -merge $(HOME)/.Xresources"
     xmonad $ defaultConfig
-        { terminal    = "xterm"        -- Solarized colorschemed
-        , modMask     = mod1Mask       -- left Alt Key
-        , borderWidth = 2
-        , focusedBorderColor = "#00FF00"
-        , normalBorderColor = "#000000"
-        , workspaces = ["term", "vim", "web", "4", "5", "6", "7", "8"]
+        { terminal    = myTerminal
+        , modMask     = myModMask       -- left Alt Key
+        , borderWidth = myBorderWidth
+        , focusedBorderColor = myFocusedBorderColor
+        , normalBorderColor = myNormalBorderColor
+        , workspaces = myWorkspaces
         , manageHook  = manageDocks <+> manageHook defaultConfig
-        , layoutHook  = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook  = myLayouts
         , logHook     = dynamicLogWithPP xmobarPP
                             { ppOutput = hPutStrLn xmproc
                             , ppTitle  = xmobarColor "green" "" . shorten 50
