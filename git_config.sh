6bfa9ceefa862682f4951fd0894b31e45c0fbab6
Siva Prasad Varma <sivapvarma@gmail.com>
2016-01-01 13:21:41 -0800

    adds github username setup

--- git_config.sh
+++ git_config.sh
@@ -3,3 +3,6 @@ git config --global user.email sivapvarma@gmail.com
 
 # set vim as text editor used by commands like git commit
 git config --global core.editor vim
+
+# setup github username
+git config --global github.user "sivapvarma"
