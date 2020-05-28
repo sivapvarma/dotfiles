2f27925a3ac2af1210ca577bfe89ecf2897051e6
Siva Prasad Varma Chiluvuri <sivapvarma@gmail.com>
2020-05-28 11:13:00 -0700

    use nvim instead of vim as git core editor
    this was having some incompatibility issues due to deoplete.

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,8 +1,9 @@
-git config --global user.name "Siva Prasad Varma"
+git config --global user.name "Siva Prasad Varma Chiluvuri"
 git config --global user.email sivapvarma@gmail.com
 
 # set vim as text editor used by commands like git commit
-git config --global core.editor vim
+# git config --global core.editor vim
+git config --global core.editor nvim
 
 # setup github username
 git config --global github.user "sivapvarma"
